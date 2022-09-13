6c2b4d32a94aa53616ce50cd708ff8784e94122a
Paula Rodrigo Martin <paulilokiestudia@gmail.com>
2022-09-13 17:16:21 +0200

    3 more done

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,8 @@
 
-# Print the pattern
+my_list = [] #create an empty list, gonna fill it with the for loop
+for i in range(1,6):
+    my_list.append(str("*")) #append to the empty list a string * from the iteration
+    print(' '.join(my_list)) 
+for i in range(1,6):
+    my_list.remove(str("*")) #remove to the empty list a string * from the iteration
+    print(' '.join(my_list)) 
--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,2 +1,8 @@
 
 # Print the numbers described in the exercise
+my_list = [] #create an empty list, gonna fill it with the for loop
+for i in range(1,11):
+    my_list.append(str(i)) #append to the empty list a string i from the iteration
+    print(' '.join(my_list)) #' '.join to separate the values, in this case by a space
+
+#print(my_list) if I run that one ot presents it as a list separated by commas an ''
