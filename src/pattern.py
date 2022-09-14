582c6eda07adaf328bae6cb28e04775f49092b5b
Paula Rodrigo Martin <paulilokiestudia@gmail.com>
2022-09-14 15:11:14 +0200

    done

--- src/pattern.py
+++ src/pattern.py
@@ -3,6 +3,6 @@ my_list = [] #create an empty list, gonna fill it with the for loop
 for i in range(1,6):
     my_list.append(str("*")) #append to the empty list a string * from the iteration
     print(' '.join(my_list)) 
-for i in range(1,6):
+for i in range(1,5):
     my_list.remove(str("*")) #remove to the empty list a string * from the iteration
     print(' '.join(my_list)) 
