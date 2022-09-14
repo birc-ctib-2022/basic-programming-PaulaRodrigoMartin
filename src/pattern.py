7301aeb92b80c957c9483b34329eec79a0c4fc0d
PaulaRodrigoMartin <112614977+PaulaRodrigoMartin@users.noreply.github.com>
2022-09-14 14:48:44 +0200

    worked out

--- src/pattern.py
+++ src/pattern.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 my_list = [] #create an empty list, gonna fill it with the for loop
 for i in range(1,6):
@@ -7,7 +6,3 @@ for i in range(1,6):
 for i in range(1,6):
     my_list.remove(str("*")) #remove to the empty list a string * from the iteration
     print(' '.join(my_list)) 
-=======
-
-# Print the pattern
->>>>>>> 7e9b3a649c1167b681583601e873fb8b78d8ace4
--- src/print-1-10.py
+++ src/print-1-10.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 
 # Print the numbers described in the exercise
 for i in range(1,11):
     print(i) 
-=======
-
-# Print the numbers described in the exercise
->>>>>>> 7e9b3a649c1167b681583601e873fb8b78d8ace4
