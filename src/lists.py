49fe1b455c48a8a8c3e68ba16d96d3ca2fd9ae18
Paula Rodrigo Martin <paulilokiestudia@gmail.com>
2022-09-14 16:39:54 +0200

    done

--- src/lists.py
+++ src/lists.py
@@ -9,6 +9,7 @@ x = [int(a) for a in sys.stdin.read().split()]
 # space separated format, you cannot use print(x) since that will add the
 # square brackets and the commas, but you can use
 #
+
 # print(" ".join(str(i) for i in x))
 #
 # to first translate the integers into strings, (str(i) for i in x),
@@ -42,17 +43,22 @@ if len(sys.argv) < 2:
 match sys.argv[1]:
     case "mean":
         # put your solution to the first exercise here
-        mean = "mean of x"
+        mean = sum(x)/len(x)
         print(mean)
 
     case "times":
         # Put your solution to the second exercise here
         times_three = []
+        for i in x:
+            times_three.append(i*3)
         print_list(times_three)
 
     case "even":
         # Put your solution to the third exercise here
         even = []
+        for i in x:
+            if i%2 == 0:
+                even.append(i)
         print_list(even)
 
     case _:
